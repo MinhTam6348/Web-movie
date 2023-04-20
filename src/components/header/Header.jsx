c68e29ca22ac53c1b787eda382ad279cbdb21dda
MinhTam6348 <ngominhtam1210@gmail.com>
2023-04-20 12:01:05 +0700

    finish movie-web

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
-import "./styles.scss";
+import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
-import { useLocation, useNavigate } from "react-router-dom";
-import logo from "../../assets/movix-logo.svg";
-import ContentWrapper from "./../contentWrapper/ContentWrapper";
+import { useNavigate, useLocation } from "react-router-dom";
 
-function Header() {
+import "./styles.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
+const Header = () => {
   const [show, setShow] = useState("top");
   const [lastScrollY, setLastScrollY] = useState(0);
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -40,8 +42,8 @@ function Header() {
     };
   }, [lastScrollY]);
 
-  const handlerSearchQuery = (e) => {
-    if (e.key === "Enter" && query.length > 0) {
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
       navigate(`/search/${query}`);
       setTimeout(() => {
         setShowSearch(false);
@@ -59,7 +61,7 @@ function Header() {
     setShowSearch(false);
   };
 
-  const handlerNavigation = (type) => {
+  const navigationHandler = (type) => {
     if (type === "movie") {
       navigate("/explore/movie");
     } else {
@@ -71,20 +73,21 @@ function Header() {
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
-        <div className="logo">
+        <div className="logo" onClick={() => navigate("/")}>
           <img src={logo} alt="" />
         </div>
         <ul className="menuItems">
-          <li className="menuItem" onClick={() => handlerNavigation("movie")}>
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
             Movies
           </li>
-          <li className="menuItem" onClick={() => handlerNavigation("tv")}>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
             TV Shows
           </li>
           <li className="menuItem">
             <HiOutlineSearch onClick={openSearch} />
           </li>
         </ul>
+
         <div className="mobileMenuItems">
           <HiOutlineSearch onClick={openSearch} />
           {mobileMenu ? (
@@ -102,7 +105,7 @@ function Header() {
                 type="text"
                 placeholder="Search for a movie or tv show...."
                 onChange={(e) => setQuery(e.target.value)}
-                onKeyUp={handlerSearchQuery}
+                onKeyUp={searchQueryHandler}
               />
               <VscChromeClose onClick={() => setShowSearch(false)} />
             </div>
@@ -111,6 +114,6 @@ function Header() {
       )}
     </header>
   );
-}
+};
 
 export default Header;
--- src/pages/details/Playbtn.jsx
+++ src/pages/details/Playbtn.jsx
@@ -1,30 +1,37 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import "./style.scss";
-
-import useFetch from "../../hooks/useFetch";
-import DetailsBanner from "./detailsBanner/DetailsBanner";
-import Cast from "./cast/Cast";
-import VideosSection from "./videosSection/VideosSection";
-import Similar from "./carousels/Similar";
-import Recommendation from "./carousels/Recommendation";
-
-const Details = () => {
-  const { mediaType, id } = useParams();
-  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-  const { data: credits, loading: creditsLoading } = useFetch(
-    `/${mediaType}/${id}/credits`
-  );
-
+export const PlayIcon = () => {
   return (
-    <div>
-      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
-      <Cast data={credits?.cast} loading={creditsLoading} />
-      <VideosSection data={data} loading={loading} />
-      <Similar mediaType={mediaType} id={id} />
-      <Recommendation mediaType={mediaType} id={id} />
-    </div>
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="80px"
+      height="80px"
+      viewBox="0 0 213.7 213.7"
+      enableBackground="new 0 0 213.7 213.7"
+      xmlSpace="preserve"
+    >
+      <polygon
+        className="triangle"
+        fill="none"
+        strokeWidth="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        points="73.5,62.5 148.5,105.8 73.5,149.1 "
+      ></polygon>
+      <circle
+        className="circle"
+        fill="none"
+        strokeWidth="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        cx="106.8"
+        cy="106.8"
+        r="103.3"
+      ></circle>
+    </svg>
   );
 };
-
-export default Details;
